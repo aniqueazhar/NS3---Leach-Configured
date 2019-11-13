a996fb371c6d9ca71fe4ecf24403a5d879d6213b
anique azhar <aneeqathar@gmail.com>
2019-11-13 11:03:13 +0500

    First commit

---
+++ ns-3.27/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.27-example-version3-debug', 'build/scratch/ns3.27-example-version2-debug', 'build/scratch/ns3.27-backup-debug', 'build/scratch/ns3.27-example-version1-debug', 'build/scratch/ns3.27-leach_new-debug', 'build/scratch/ns3.27-leach-helper-debug', 'build/scratch/ns3.27-30april-debug', 'build/scratch/ns3.27-leach-example-debug', 'build/scratch/subdir/ns3.27-subdir-debug', 'build/scratch/ns3.27-wifi-simple-adhoc-debug', 'build/scratch/ns3.27-test-debug', 'build/scratch/ns3.27-assignment-debug', 'build/scratch/ns3.27-leach-debug', 'build/scratch/ns3.27-27april-debug', 'build/scratch/ns3.27-scratch-simulator-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
---
+++ ns-3.27/scratch/leach-helper.cc
@@ -0,0 +1,13 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "leach-helper.h"
+using namespace ns3; 
+
+
+/* ... */
+int main () 
+{
+ return 0;
+}
+
+
